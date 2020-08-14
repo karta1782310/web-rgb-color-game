1ca1c19390897fb104cfb6bbd11dea91ff783a07
Yu-Cheng, Cheng <karta1782310@gmail.com>
2020-08-14 16:55:03 +0800

    add the gen random color func and apply it

--- colorGame.js
+++ colorGame.js
@@ -1,30 +1,51 @@
-let colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-];
-
+let colors = generateRandomColor(6);
 let squares = document.querySelectorAll(".square");
-let pickedColor = colors[3];
+let pickedColor = pickColor(colors);
 let colorDisplay = document.querySelector("#colorDisplay");
+let messageDisplay = document.querySelector("#message");
+let title = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
 for (let i = 0; i < squares.length; i++) {
-    // 初始化顏色
     squares[i].style.backgroundColor = colors[i];
-    // 新增事件
+    
     squares[i].addEventListener("click", function(){
-        // 抓取點擊顏色
         let clickedColor = this.style.backgroundColor;
-        // 比對選取顏色
-        if (clickedColor == pickedColor) {
-            alert("CORRECT!");
+
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct!";
+            title.style.backgroundColor = clickedColor;
+            changeColors(clickedColor);
         } else {
-            alert("WRONG!!!");
+            messageDisplay.textContent = "Try Again!";
+            this.style.backgroundColor = "#232323";
         }
     });      
+}
+
+function generateRandomColor(num) {
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+function pickColor(colors) {
+    let random = Math.floor(Math.random() * 6 + 1);
+    return colors[random];
+}
+
+function changeColors(color) {
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color
+    }
 }
